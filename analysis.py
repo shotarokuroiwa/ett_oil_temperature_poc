92175b4ee9f19e38caec89b9e8c4ad1b56647a7c
shotarokuroiwa <shotarokuroiwa@icloud.com>
2026-06-05 16:29:44 +0900

    add sarimax

--- analysis.py
+++ analysis.py
@@ -16,10 +16,10 @@ print(df.index.min(), df.index.max())
 print(df.isna().sum())
 
 # # ADF
-# from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import adfuller
 #
-# y = df["OT"].dropna() # NaNがあるデータを削除する
-# # y = df["OT"].diff().dropna() 非定常なら変化量の変化量を見る(diff())
+# y = df["OT"].() # NaNがあるデータを削除する
+# # y = df["OT"].diff().() 非定常なら変化量の変化量を見る(diff())
 #
 # # d = 1のみ自動
 # adf_result = adfuller(
@@ -69,13 +69,13 @@ print(df.isna().sum())
 # resid_check = y - trend - seasonal
 # print(np.nanmean(np.abs(resid_check - resid)))
 #
-# # ACF
+# # ACF PACF
 # from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 #
-# fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(14,8)) # fig = 部全体のオブジェクト, axes =　二つのグラフの配列
+# fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(14,8)) # fig = 全体のオブジェクト, axes =　二つのグラフの配列
 #
 # plot_acf(
-#     df["OT"].dropna(),
+#     df["OT"].(),
 #     lags=192, # 一周先(TtとTt-s)のデータに回帰性がないか
 #     alpha=0.05, # 有意水準
 #     ax=axes[0], #描画先
@@ -97,118 +97,201 @@ print(df.isna().sum())
 
 
 # 最大最小スケーリング
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import StandardScaler
+#
+# minmax_scaler = MinMaxScaler(feature_range=(0.0, 1.0), clip=False)
+# scaled_values = minmax_scaler.fit_transform(df[feature_cols])
+# scaled_df = pd.DataFrame(scaled_values, index=df.index, columns=feature_cols)
+#
+# print(scaled_df.head())
+# print(minmax_scaler.data_min_)
+# print(minmax_scaler.data_max_)
+#
+# # 標準化
+# standard_scaler = StandardScaler(with_mean=True, with_std=True)
+# standard_values = standard_scaler.fit_transform(df[feature_cols])
+# standard_df = pd.DataFrame(standard_values, index=df.index, columns=feature_cols)
+#
+# print(standard_df.head())
+# print(standard_scaler.mean_)
+# print(standard_scaler.scale_)
+#
+# # 外れ値の影響を数値化
+# x = np.array([[10.0], [11.0], [12.0], [13.0], [100.0]])
+#
+# minmax = MinMaxScaler(feature_range=(0, 1))
+# standard = StandardScaler()
+#
+# print("\nMinMax: ", minmax.fit_transform(x).ravel())
+# print("Standard: ", standard.fit_transform(x).ravel()) #　標準化は外れ値が存在しても、間隔は担保している
+#
+#
+# # テンソル
+# def make_sliding_windows_numpy(
+#     data: np.ndarray, # 全サンプルデータ
+#     target: np.ndarray, # 予測対象
+#     input_length: int, # AR(p)のp
+#     forecast_horizon: int # 何ステップ予測するか
+# ) -> tuple[np.ndarray, np.ndarray]:
+#     x_list = [] # 行
+#     y_list = [] # 列
+#     total_length = len(data)
+#     max_start = total_length - input_length - forecast_horizon + 1
+#
+#     # 一回のループである時間t, t-1, t-2 × 説明変数 の行列ができる
+#     for start_idx in range(max_start):
+#         end_idx = start_idx + input_length
+#         target_end_idx = end_idx + forecast_horizon
+#         x_window = data[start_idx:end_idx, :] # 左側: a:bでa~bまでの行, 右側: :ですべての列
+#         y_window = target[end_idx:target_end_idx]
+#         x_list.append(x_window)
+#         y_list.append(y_window)
+#
+#     # この時点でx_listはp × 特徴量の行列の配列のなっている
+#     x = np.stack(x_list, axis=0) # stackはテンソルにし、[ループ数, 行, 列]でアクセスできる
+#     y = np.stack(y_list, axis=0)
+#     return x, y
+#
+# input_length = 96
+# forecast_horizon = 24
+#
+# scaler = StandardScaler()
+# values_scaled = scaler.fit_transform(df[feature_cols].values) # .values = 見出しを取り除いて、純粋は配列だけを返す
+# target_values = df[target_col].values
+#
+# x_np, y_np = make_sliding_windows_numpy(
+#     data=values_scaled,
+#     target=target_values,
+#     input_length=input_length,
+#     forecast_horizon=forecast_horizon
+# )
+#
+# print("\nx_np shalpe: ", x_np.shape) # .shape = テンソルの大きさを[z, x, y]で出力
+# print("y_np shalpe: ", y_np.shape)
+# print("一回目, 2行目, 4列目: ", x_np[0, 1, 4]) 
+#
+# """
+# pytorchによるテンソル化
+#
+# """
+#
+# # ラグ特徴量(sarimaxのように時系列をから推測するモデル用に配列を整形)
+# df_feat = df.copy()
+# lag_step = [1, 2] # AR(2)
+# for i in lag_step:
+#     df_feat[f"OT_lag_{i}"] = df_feat["OT"].shift(i) # .shift = OT列のi個前の値そのものを取ってくる
+#
+# # 差分特徴量
+# dif_steps = [1, 4, 96]
+# ot_history = df_feat["OT"].shift(1)
+# for step in dif_steps:
+#     df_feat[f"OT_diff_{step}"] = ot_history.diff(periods=step) # .diff = step個前の値との差分をとる
+#
+# # 移動平均特徴量
+# rolling_windows = [4, 16, 96]
+# ot_history = df_feat["OT"].shift(1)
+# for window in rolling_windows:
+#     # rolling() = windowの数だけ今いる行の上から値をそのまま持ってくる　複数行持ってくる.shift()
+#     df_feat[f"OT_rooll_mean_{window}"] = ot_history.rolling(window=window, min_periods=window).mean() # mena() = 平均
+#     df_feat[f"OT_rooll_std_{window}"] = ot_history.rolling(window=window, min_periods=window).std() # std() = 標準偏差
+#
+# print("\n",df_feat.head(20))
+#
+# # 周期特徴量(時間をラジアンに変換)
+# df["minute"] = df.index.minute
+# df["hour"] = df.index.hour
+# df["dayofweek"] = df.index.dayofweek
+#
+# total_munutes = df.index.hour * 60 + df.index.minute
+# df["minute_sin"] = np.sin(2.0 * np.pi * df["minute"] / 60.0) # sinとconに分けるベクトルを一マスに入れれないから
+# df["minute_cos"] = np.cos(2.0 * np.pi * df["minute"] / 60.0)
+# df["hour_sin"] = np.sin(2.0 * np.pi * total_munutes / 1440.0)
+# df["hour_cos"] = np.cos(2.0 * np.pi * total_munutes / 1440.0)
+# df["dayofweek_sin"] = np.sin(2.0 * np.pi * df["dayofweek"] / 7.0)
+# df["dayofweek_cos"] = np.cos(2.0 * np.pi * df["dayofweek"] / 7.0)
+#
+# print(df.head(30))
+#
+"""
+SARIMAX
+"""
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.stattools import acf, pacf
 
-minmax_scaler = MinMaxScaler(feature_range=(0.0, 1.0), clip=False)
-scaled_values = minmax_scaler.fit_transform(df[feature_cols])
-scaled_df = pd.DataFrame(scaled_values, index=df.index, columns=feature_cols)
+def estimate_d_by_adf(series: pd.Series, max_d: int = 2, alpha: float = 0.05) -> int:
+    current = series.dropna()
+    for d in range(max_d + 1):
+        result = adfuller(current, maxlag=None, regression="c", autolag="AIC")
+        p_value = result[1]
+        print(f"d={d}, p-value={p_value}")
+        if p_value < alpha:
+            return d
+        current = current.diff().dropna()
+    return max_d
 
-print(scaled_df.head())
-print(minmax_scaler.data_min_)
-print(minmax_scaler.data_max_)
+# (1 - L)ᵈ
+d = estimate_d_by_adf(df["OT"], max_d=2, alpha=0.05) # 何回変化量をとったか(定常になるまで)
+s = 96
 
-# 標準化
-standard_scaler = StandardScaler(with_mean=True, with_std=True)
-standard_values = standard_scaler.fit_transform(df[feature_cols])
-standard_df = pd.DataFrame(standard_values, index=df.index, columns=feature_cols)
+# (1 - L⁹⁶)ᵒ
+seasonal_diff = df["OT"].diff(periods=s).dropna() # diff() = 一周期前のデータを引くと季節やトレンドの波を消える
+seasonal_p_value = adfuller(seasonal_diff, maxlag=None, regression="c", autolag="AIC")[1] # p値
+D = 1 if seasonal_p_value < 0.05 else 0
 
-print(standard_df.head())
-print(standard_scaler.mean_)
-print(standard_scaler.scale_)
+# 定常化
+stationary_data = df["OT"].copy() 
+for _ in range(d):
+    stationary_data = stationary_data.diff()
+stationary_data = stationary_data.dropna() # 空白の行を削除
 
-# 外れ値の影響を数値化
-x = np.array([[10.0], [11.0], [12.0], [13.0], [100.0]])
+# acf, pacf
+acf_values = acf(stationary_data, nlags=20, fft=True) # acf()は配列を返す ⇔  plot_acf()はグラフを返す
+pacf_values = pacf(stationary_data, nlags=20, method="ywm")
 
-minmax = MinMaxScaler(feature_range=(0, 1))
-standard = StandardScaler()
+print("PACF_VALUE = \n", pacf_values)
 
-print("\nMinMax: ", minmax.fit_transform(x).ravel())
-print("Standard: ", standard.fit_transform(x).ravel()) #　標準化は外れ値が存在しても、間隔は担保している
+threshold = 1.96 / np.sqrt(len(stationary_data)) # 信頼区間(これより外に出ると偶然のノイズではない)
+print("信頼区間", threshold)
 
+p_candidates = [lag for lag in range(1, 6) if abs(pacf_values[lag]) > threshold] 
+q_candidates = [lag for lag in range(1, 6) if abs(acf_values[lag]) > threshold]
+p = max(p_candidates) if p_candidates else 1
+q = max(q_candidates) if q_candidates else 1
 
-# テンソル
-def make_sliding_windows_numpy(
-    data: np.ndarray, # 全サンプルデータ
-    target: np.ndarray, # 予測対象
-    input_length: int, # AR(p)のp
-    forecast_horizon: int # 何ステップ予測するか
-) -> tuple[np.ndarray, np.ndarray]:
-    x_list = [] # 行
-    y_list = [] # 列
-    total_length = len(data)
-    max_start = total_length - input_length - forecast_horizon + 1
+print("\norder", (p, d, q))
+print("seasonal_order", (1, D, 1, 96))
 
-    # 一回のループである時間t, t-1, t-2 × 説明変数 の行列ができる
-    for start_idx in range(max_start):
-        end_idx = start_idx + input_length
-        target_end_idx = end_idx + forecast_horizon
-        x_window = data[start_idx:end_idx, :] # 左側: a:bでa~bまでの行, 右側: :ですべての列
-        y_window = target[end_idx:target_end_idx]
-        x_list.append(x_window)
-        y_list.append(y_window)
+y = df["OT"].asfreq("15min").interpolate(method="time")
+exog_cols = ["HUFL", "HULL", "MUFL", "MULL", "LUFL", "LULL"] # 外部変数
+exog = df[exog_cols].asfreq("15min").interpolate(method="time")
 
-    # この時点でx_listはp × 特徴量の行列の配列のなっている
-    x = np.stack(x_list, axis=0) # stackはテンソルにし、[ループ数, 行, 列]でアクセスできる
-    y = np.stack(y_list, axis=0)
-    return x, y
+# 訓練用8割, 答え合わせ用2割
+train_size = int(len(y) * 0.8)
+# 目的変数
+y_train = y.iloc[:train_size] # :a = indexがaまで 
+y_test = y.iloc[train_size:] # a: = indexがaから最後まで
+exog_train = exog.iloc[:train_size]
+exog_test = exog.iloc[:train_size:]
 
-input_length = 96
-forecast_horizon = 24
-
-scaler = StandardScaler()
-values_scaled = scaler.fit_transform(df[feature_cols].values) # .values = 見出しを取り除いて、純粋は配列だけを返す
-target_values = df[target_col].values
-
-x_np, y_np = make_sliding_windows_numpy(
-    data=values_scaled,
-    target=target_values,
-    input_length=input_length,
-    forecast_horizon=forecast_horizon
+# SIRIMAX()
+model = SARIMAX(
+    endog=y_train,
+    exog=exog_train,
+    order=(p, d, q), # 次数(自己回帰, 変化量の変化量,　移動平均)
+    seasonal_order=(1, D, 1, 96,), # (一期前の変化量の重み, D, 一期前のノイズの重み, 周期)
+    trend="c",
+    enforce_stationarity=False,
+    enforce_invertibility=False
 )
 
-print("\nx_np shalpe: ", x_np.shape) # .shape = テンソルの大きさを[z, x, y]で出力
-print("y_np shalpe: ", y_np.shape)
-print("一回目, 2行目, 4列目: ", x_np[0, 1, 4]) 
+# model学習
+# fit() = 微分, 二回微分を実行し最適された重みの確定値を出力
+result = model.fit(disp=False, maxiter=200) # maxiter = 何回重み更新を行うか
+print(result.summary()) # 最適化した重みのレポート
 
-"""
-pytorchによるテンソル化
+# model評価
+forecast_reuslt = result.get_forecast(steps=len(y_test), exog=exog_test) # 
+y_pred_sarimax = forecast_reuslt.predicted_mean() # 確率分布で一番確率が高い予測値
+conf_int = forecast_reuslt.conf_int(alpha=0.05) # 95%の確率でこの範囲に収まるという信頼区間(最大最小)
 
-"""
-
-# ラグ特徴量(sarimaxのように時系列をから推測するモデル用に配列を整形)
-df_feat = df.copy()
-lag_step = [1, 2] # AR(2)
-for i in lag_step:
-    df_feat[f"OT_lag_{i}"] = df_feat["OT"].shift(i) # .shift = OT列のi個前の値そのものを取ってくる
-
-# 差分特徴量
-dif_steps = [1, 4, 96]
-ot_history = df_feat["OT"].shift(1)
-for step in dif_steps:
-    df_feat[f"OT_diff_{step}"] = ot_history.diff(periods=step) # .diff = step個前の値との差分をとる
-
-# 移動平均特徴量
-rolling_windows = [4, 16, 96]
-ot_history = df_feat["OT"].shift(1)
-for window in rolling_windows:
-    # rolling() = windowの数だけ今いる行の上から値をそのまま持ってくる　複数行持ってくる.shift()
-    df_feat[f"OT_rooll_mean_{window}"] = ot_history.rolling(window=window, min_periods=window).mean() # mena() = 平均
-    df_feat[f"OT_rooll_std_{window}"] = ot_history.rolling(window=window, min_periods=window).std() # std() = 標準偏差
-
-print("\n",df_feat.head(20))
-
-# 時間をラジアンに変換
-df["minute"] = df.index.minute
-df["hour"] = df.index.hour
-df["dayofweek"] = df.index.dayofweek
-
-total_munutes = df.index.hour * 60 + df.index.minute
-df["minute_sin"] = np.sin(2.0 * np.pi * df["minute"] / 60.0) # sinとconに分けるベクトルを一マスに入れれないから
-df["minute_cos"] = np.cos(2.0 * np.pi * df["minute"] / 60.0)
-df["hour_sin"] = np.sin(2.0 * np.pi * total_munutes / 1440.0)
-df["hour_cos"] = np.cos(2.0 * np.pi * total_munutes / 1440.0)
-df["dayofweek_sin"] = np.sin(2.0 * np.pi * df["dayofweek"] / 7.0)
-df["dayofweek_cos"] = np.cos(2.0 * np.pi * df["dayofweek"] / 7.0)
-
-print(df.head(30))
